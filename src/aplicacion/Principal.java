d0ce9a7751dbef4c99235b1d3730b3f4f643abf6
AlejandroLilloRodriguez <alejandro.lillorodriguez@usp.ceu.es>
2023-02-17 09:03:47 +0100

    Ejercicio12 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,14 @@
-package aplicacion;public class Principal {
+package aplicacion;
+
+import dominio.Ejercicio12;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Inserte el numero del que quiera calcular su fibonacci: ");
+        int numero= sc.nextInt();
+        System.out.println("El fibonacci de " + numero + " es: " + Ejercicio12.fibonacci(numero));
+    }
 }
--- src/dominio/Ejercicio12.java
+++ src/dominio/Ejercicio12.java
@@ -1,2 +1,11 @@
-package dominio;public class Ejercicio12 {
-}
+package dominio;
+
+public class Ejercicio12 {
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return 0;
+        } else
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
